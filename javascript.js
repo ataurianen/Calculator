2773e689dc5ba3949bc8401855f2dfa301279ee0
Andrew Taurianen <andrew.taurianen@gmail.com>
2023-01-28 13:54:48 -0500

    Added operate function

--- javascript.js
+++ javascript.js
@@ -18,3 +18,21 @@ function divide(num1, num2) {
         return "error";
     }
 }
+
+function operate(operator, num1, num2) {
+    if (operator == '+') {
+        return add(num1,num2);
+    }
+    else if (operator == '-') {
+        return subtract(num1,num2);
+    }
+    else if (operator == '*') {
+        return multiply(num1,num2);
+    }
+    else if (operator == '/') {
+        return divide(num1,num2);
+    }
+    else {
+        return 'Invalid operator';
+    }
+}
